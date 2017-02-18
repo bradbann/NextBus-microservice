c2a2acd43bc0cf41a9e65ffdae1fb347285fe6ec
Pedro <csixteen@gmail.com>
2017-02-18 13:52:24 +0100

    Some fixes here and there

--- nextbus/nextbus.sh
+++ nextbus/nextbus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-IP_ADDR=$(ifconfig eth0 | grep "inet addr" | awk '{ print $2 }' | cut -d : -f 2)
+IP_ADDR=$(ifconfig eth0 | grep "inet addr" | awk '{ print $2 }' \
+  | cut -d : -f 2)
 
 curl http://etcd:2379/v2/keys/services/nextbus/servers/$HOSTNAME \
   -d value='{"host":"'$IP_ADDR'", "port": 5000}' -d ttl=90 -X PUT
